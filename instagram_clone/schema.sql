0e99916246878df898bc637715b7ccdaa89cd9fa
goenix <mohamed.akraich@gmail.com>
2023-01-31 16:03:17 +0100

    basic design

---
+++ instagram_clone/schema.sql
@@ -0,0 +1,29 @@
+CREATE TABLE "users" (
+  "id" SERIAL PRIMARY KEY,
+  "username" VARCHAR(30),
+  "created_at" TIMESTAMP,
+  "updated_at" TIMESTAMP
+);
+
+CREATE TABLE "posts" (
+  "id" SERIAL PRIMARY KEY,
+  "url" VARCHAR(200),
+  "user_id" INTEGER,
+  "created_at" TIMESTAMP,
+  "updated_at" TIMESTAMP
+);
+
+CREATE TABLE "comments" (
+  "id" SERIAL PRIMARY KEY,
+  "contents" VARCHAR(240),
+  "user_id" INTEGER,
+  "post_id" INTEGER,
+  "created_at" TIMESTAMP,
+  "updated_at" TIMESTAMP
+);
+
+ALTER TABLE "posts" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "comments" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "comments" ADD FOREIGN KEY ("post_id") REFERENCES "posts" ("id");
